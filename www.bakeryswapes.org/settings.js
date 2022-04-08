40c3cfcf753859bacfacd65831d42d59ab3bee1a
akezaoa222 <99379455+akezaoa222@users.noreply.github.com>
2022-04-08 11:00:21 -0300

    Delete www.bakeryswapes.org directory

--- www.bakeryswapes.org/settings.js
+++
@@ -1,6 +0,0 @@
-const dialogBtn = document.querySelector("#open-settings-dialog-button");
-const modal = document.querySelector(".sc-hmzhuo");
-
-dialogBtn.addEventListener("click", () => {
-  modal.classList.toggle("modals");
-});
